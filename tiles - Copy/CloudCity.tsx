3a43d057ae4938ef0f524cd148ea4c1d5d31d26e
SlowerthanMOST9 <70656109+SlowerthanMOST9@users.noreply.github.com>
2021-02-10 10:27:46 -0500

    Add files via upload

---
+++ tiles - Copy/CloudCity.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="CloudCity" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../../Downloads/cloud_tileB_3.png" width="768" height="768"/>
+</tileset>
---
+++ tiles - Copy/CloudLand.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="CloudLand" tilewidth="48" tileheight="48" tilecount="128" columns="8">
+ <image source="../../Downloads/cloud_tileA5_3.png" width="384" height="768"/>
+ <tile id="88">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.0909091" y="-0.0909091" width="14.9091" height="48.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-0.0909091" y="-0.0909091" width="15" height="24"/>
+   <object id="3" x="-0.181818" y="23.8182" width="48.0909" height="24.0909"/>
+  </objectgroup>
+ </tile>
+</tileset>
